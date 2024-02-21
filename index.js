876d6cb2c10f778e412648497260fedf8d79766c
Aleksandrova Olga <bulbenko@yandex.ru>
2024-02-21 14:57:10 +0100

    init: project

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import { httpServer } from "./src/http_server/index.js";
+import { httpServer } from './src/http_server/index.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const HTTP_PORT = 8181;
-
+// eslint-disable-next-line no-undef
+const HTTP_PORT = process.env.PORT || 8181;
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
